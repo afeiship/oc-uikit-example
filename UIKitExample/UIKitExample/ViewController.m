85fb74182583b844d55a78aca173554137b51710
afei <88603982@qq.com>
2016-06-19 01:27:15 +0800

    update structure

--- UIKitExample/UIKitExample/ViewController.m
+++ UIKitExample/UIKitExample/ViewController.m
@@ -17,6 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"viewDidLoad!");
     [self buttonExample];
     // Do any additional setup after loading the view, typically from a nib.
 }
